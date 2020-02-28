07fe87569bea6eedd4085c1f17026a2a30b41db4
WolfTheZelda <oliveiralima@outlook.com>
2020-02-28 17:54:01 -0300

    Fixed 'timestamp' error

--- lib/cloud_firestore_all.dart
+++ lib/cloud_firestore_all.dart
@@ -4,4 +4,5 @@ export 'src/interface.dart'
     if (dart.library.io) 'src/mobile.dart'
     if (dart.library.js) 'src/web.dart';
 
-export 'src/timestamp.dart';
+//export 'src/timestamp.dart';
+export 'package:cloud_firestore_platform_interface/src/timestamp.dart';
